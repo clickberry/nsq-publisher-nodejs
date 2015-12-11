59a8c2e27b0e0730afa2bcbe7411ddfc6d97b31c
altuhov <altuhov@clickberry.com>
2015-12-11 15:22:58 +0300

    Added reconnect Added message resend logic for failed publish or disconnect

--- index.js
+++ index.js
@@ -3,53 +3,141 @@ var util = require('util');
 var nsq = require('nsqjs');
 var q = require('q');
 
-function Bus(options) {
-    options = options || {};
-    var bus = this;
-    bus._deferred = q.defer();
+var retryCount = 0;
+
+function Publisher(options) {
+    var self = this;
+    this._options = options = initialize(options || {});
+    this._deferred = q.defer();
+    this._messages = [];
 
     events.EventEmitter.call(this);
 
     // Stub for development
     if (options.mode === 'development') {
-        bus._bus = {
-            publish: function (topic, message, callback) {
-                console.log('topic: ' + topic);
-                console.log(message);
-                callback()
-            }
+        self.publish = function (topic, message, callback) {
+            console.log('topic: ' + topic);
+            console.log(message);
+            callback()
         };
         console.warn('NSQ is not using! Please change "development" mode.');
-        bus._deferred.resolve();
+        self._deferred.resolve();
         return;
     }
 
     // connect to the Bus
-    var busClient = new nsq.Writer(options.address, options.port);
+    self._bus = createBusClient();
+    self._bus.connect();
 
-    bus._bus = busClient;
-    busClient.connect();
-    busClient.on('ready', function () {
-        console.log('ServiceBus is started.');
-        bus._deferred.resolve();
-    });
-    busClient.on('error', function (err) {
-        console.log(err);
-        bus._deferred.reject(err);
-    });
+    function createBusClient() {
+        var busClient = new nsq.Writer(self._options.address, self._options.port);
+
+        busClient.on('ready', function () {
+            console.log('ServiceBus is started.');
+            self._hasConnection = true;
+            self._deferred.resolve();
+            retryCount = 0;
+
+            if (self._messages.length > 0) {
+                console.log('Resend messages.');
+                self.sendMessages();
+            }
+        });
+
+        busClient.on('error', function (err) {
+            console.log(err);
+        });
+
+        busClient.on('closed', function () {
+            console.log('NSQ Writer connection was closed.');
+            self._hasConnection = false;
+
+            if (retryCount < self._options.retryCount) {
+                setTimeout(function () {
+                    self._deferred = q.defer();
+                    self._bus = createBusClient();
+                    self._bus.connect();
+                }, self._options.retryTimeout * ((self._options.retryRate - 1) * retryCount + 1));
+                retryCount++;
+            } else {
+                var err = new Error('Can not reconnect to NSQ.');
+                self._deferred.reject(err);
+                self.emit('reconnect_failed', err);
+            }
+        });
+
+        return busClient;
+    }
 }
 
-util.inherits(Bus, events.EventEmitter);
+function initialize(options) {
+    options.retryTimeout = options.retryTimeout > 0 ? options.retryTimeout : 1000;
+    options.retryCount = options.retryCount > 0 ? options.retryCount : 10;
+    options.retryRate = options.retryRate > 0 ? options.retryRate : 1;
+    options.attemptCount = options.attemptCount > 0 ? options.attemptCount : 3;
+    options.attemptTimeout = options.attemptTimeout > 0 ? options.attemptTimeout : 1000;
 
-Bus.prototype.publish = function () {
-    var args = arguments;
-    var callback = arguments[arguments.length - 1];
-    var bus = this._bus;
-    this._deferred.promise.then(function () {
-        bus.publish.apply(this, args);
+    return options;
+}
+
+util.inherits(Publisher, events.EventEmitter);
+
+Publisher.prototype.sendMessages = function () {
+    var self = this;
+    var messages = this._messages;
+
+    self._deferred.promise.then(function () {
+        while (messages.length > 0 && self._hasConnection) {
+            var msg = messages.shift();
+            (function (msg) {
+                self._bus.publish(msg.topic, msg.message, function (err) {
+                    if (err) {
+                        if (!self._options.attemptCount || msg.attemptCount < self._options.attemptCount) {
+                            msg.attemptCount++;
+                            console.log('Publish error: ' + err.message);
+                            console.log('Attempts count: ' + msg.attemptCount);
+                            console.log(msg.message);
+
+                            setTimeout(function () {
+                                self.publish(msg.topic, msg.message, msg.callback);
+                            }, self._options.attemptTimeout)
+                        } else {
+                            msg.callback(err);
+                        }
+
+                        return;
+                    }
+
+                    msg.callback(null);
+                });
+            })(msg);
+        }
     }, function (err) {
-        callback(err);
+        console.log('Can not send messages.')
     });
 };
 
-module.exports = Bus;
+Publisher.prototype.publish = function (topic, message, callback) {
+    var self = this;
+    var msg = createMessaage(topic, message, callback);
+    self._messages.push(msg);
+    self.sendMessages();
+};
+
+function createMessaage(topic, message, callback) {
+    return {
+        topic: topic,
+        message: message,
+        callback: callback,
+        attemptCount: 0
+    };
+}
+
+module.exports = Publisher;
+
+//################################333
+
+
+
+//################################333
+
--- test/publisher-test.js
+++ test/publisher-test.js
@@ -7,25 +7,23 @@ describe('Publish function', function () {
         publisher.publish('my topic', 'my message', done);
     });
 
-    it('should err', function (done) {
-        var publisher = new Publisher();
-        publisher.publish('my topic', 'my message', function(err){
-            if(err){
-                return done();
-            }
+    it('should emit reconnect error', function (done) {
+        var publisher = new Publisher({retryCount:1, retryTimeout: 100});
+        publisher.on('reconnect_failed', function(err){
+            done();
         });
     });
 });
 
 describe('Inherit', function () {
-    function Bus(options){
+    function Bus(options) {
         Publisher.call(this, options);
     }
 
     util.inherits(Bus, Publisher)
 
-    Bus.prototype.myMessagePub= function (message, callback) {
-        this._bus.publish('test-topic', message, function (err) {
+    Bus.prototype.myMessagePub = function (message, callback) {
+        this.publish('test-topic', message, function (err) {
             if (err) {
                 return callback(err);
             }
@@ -34,7 +32,7 @@ describe('Inherit', function () {
     };
 
     it('should send message', function (done) {
-        var bus=new Bus({mode: 'development'});
+        var bus = new Bus({mode: 'development'});
         bus.myMessagePub('my message', done)
     });
 });
